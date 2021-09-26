e9f67a5daa0e700bce3d88a8f4b3080d41d6facf
Solmn <solomonmolla10@gmail.com>
2021-09-26 09:47:05 -0500

    Added dependencies and Kafka configuration params

---
+++ twitter-producer/src/test/java/com/miu/cs523/twitterproducer/TwitterProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.cs523.twitterproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
